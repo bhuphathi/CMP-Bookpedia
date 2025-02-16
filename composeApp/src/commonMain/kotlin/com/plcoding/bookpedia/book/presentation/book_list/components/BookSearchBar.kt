accdaf79e880120bdf48fb707957412838ea418f
bhuphathi <bhuphathi@gmail.com>
2025-02-16 13:40:16 +0530

    feat: text selection color

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
@@ -3,6 +3,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
@@ -14,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -32,40 +35,50 @@ fun BookSearchBar(
     onImeSearch: () -> Unit = {},
     modifier: Modifier = Modifier,
 ) {
-    OutlinedTextField(
-        value = searchQuery,
-        onValueChange = onSearchQueryChange,
-        shape = RoundedCornerShape(100),
-        colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = DarkBlue, focusedBorderColor = SandYellow
-        ),
-        placeholder = {
-            Text(text = stringResource(Res.string.search_hint))
-        },
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+    CompositionLocalProvider(
+        // 'provides' is an infix function can also be written like this ==> LocalTextSelectionColors provides TextSelectionColors
+        LocalTextSelectionColors.provides(
+            TextSelectionColors(
+                handleColor = DarkBlue, backgroundColor = SandYellow
             )
-        },
-        singleLine = true,
-        keyboardActions = KeyboardActions(onSearch = {
-            onImeSearch()
-        }),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text, imeAction = ImeAction.Search
-        ),
-        trailingIcon = {
-            AnimatedVisibility(visible = searchQuery.isNotEmpty()) {
+        )
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            shape = RoundedCornerShape(100),
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = DarkBlue, focusedBorderColor = SandYellow
+            ),
+            placeholder = {
+                Text(text = stringResource(Res.string.search_hint))
+            },
+            leadingIcon = {
                 Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = stringResource(Res.string.clear_search),
-                    tint = MaterialTheme.colorScheme.onSurface
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
                 )
-            }
-        },
-        modifier = modifier.background(shape = RoundedCornerShape(100), color = DesertWhite)
-            .minimumInteractiveComponentSize(),
-    )
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(onSearch = {
+                onImeSearch()
+            }),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text, imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(visible = searchQuery.isNotEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = stringResource(Res.string.clear_search),
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            },
+            modifier = modifier.background(shape = RoundedCornerShape(100), color = DesertWhite)
+                .minimumInteractiveComponentSize(),
+        )
+
+    }
 }
