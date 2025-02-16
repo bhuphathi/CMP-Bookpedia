471f38316d46524c834c3b711f7efedd46aa8f9c
bhuphathi <bhuphathi@gmail.com>
2025-02-16 14:19:10 +0530

    book list screen impl

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -1,10 +1,22 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
+import BookSearchBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.bookpedia.book.domain.Book
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.core.presentation.DarkBlue
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -24,5 +36,21 @@ fun BookListScreenRoot(
 
 @Composable
 private fun BookListScreen(state: BookListState, onAction: (BookListAction) -> Unit) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = DarkBlue).statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            },
+            onImeSearch = {
+                keyboardController?.hide()
+            },
+            modifier = Modifier.widthIn(max = 400.dp).fillMaxWidth().padding(16.dp)
+        )
+    }
 
 }
