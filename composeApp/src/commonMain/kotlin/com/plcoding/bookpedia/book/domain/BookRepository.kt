1db6015a5dfd0ce6b9c0ee5d7f48bde7b99eb726
bhuphathi <bhuphathi@gmail.com>
2025-02-20 09:23:10 +0530

    book repository interface added in domain

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/BookRepository.kt
@@ -0,0 +1,11 @@
+package com.plcoding.bookpedia.book.domain
+
+import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.Result
+
+/**
+ * [https://youtu.be/WT9-4DXUqsM?t=8682](https://youtu.be/WT9-4DXUqsM?t=8682)
+ */
+interface BookRepository {
+    suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote>
+}
