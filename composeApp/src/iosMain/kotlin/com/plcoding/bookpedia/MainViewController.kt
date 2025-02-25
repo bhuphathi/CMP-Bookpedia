f7325b05d64247a2b25b17baee02b053ebb89303
bhuphathi <bhuphathi@gmail.com>
2025-02-25 07:02:50 +0530

    mv app.kt to app module

--- composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/MainViewController.kt
@@ -1,8 +1,8 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.app.App
 import com.plcoding.bookpedia.di.initKoin
-import io.ktor.client.engine.darwin.Darwin
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
