1bc352dc50cf07954a7b02b1641e6a998a710edc
bhuphathi <bhuphathi@gmail.com>
2025-02-25 13:48:02 +0530

    fix: loading state of book list

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -10,7 +10,7 @@ data class BookListState(
     val searchQuery: String = "kotlin",
     val searchResults: List<Book> = emptyList(),
     val favoriteBooks: List<Book> = emptyList(),
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null,
 )
